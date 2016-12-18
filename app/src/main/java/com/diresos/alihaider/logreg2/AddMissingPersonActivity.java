9c88d7839f6e924addb8b6611b3c68443d30cff2
Ali Haider <aliaiden@yahoo.com>
2016-12-18 14:09:39 +0500

    Made quick post form and add missing person form.

--- app/src/main/java/com/diresos/alihaider/logreg2/AddMissingPersonActivity.java
+++ app/src/main/java/com/diresos/alihaider/logreg2/AddMissingPersonActivity.java
@@ -1,13 +1,55 @@
 package com.diresos.alihaider.logreg2;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AddMissingPersonActivity extends AppCompatActivity {
+    ImageView missingPersonPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_missing_person);
+
+        missingPersonPic = (ImageView) findViewById(R.id.iv_missing_person_pic);
+        missingPersonPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
+
+            Uri pickedImage = data.getData();
+            // Let's read picked image path using content resolver
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor cursor = this.getContentResolver().query(pickedImage, filePath, null, null, null);
+            cursor.moveToFirst();
+            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap galleryPic = BitmapFactory.decodeFile(imagePath, options);
+            missingPersonPic.setImageBitmap(galleryPic);
+
+            // Do something with the bitmap
+
+            // At the end remember to close the cursor or you will end with the RuntimeException!
+            cursor.close();
+        }
     }
 }
