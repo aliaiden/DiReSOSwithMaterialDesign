2c0895efcb7e6e351fb007774e4145b5df0cfa20
Ali Haider <aliaiden@yahoo.com>
2016-12-28 10:26:09 +0500

    Added search bar for missing persons

--- app/src/main/java/com/diresos/alihaider/logreg2/MissingPersonFragment.java
+++ app/src/main/java/com/diresos/alihaider/logreg2/MissingPersonFragment.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.app.Fragment;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -15,10 +17,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.R.id.content;
 import static android.R.id.list;
 
 
@@ -33,6 +37,7 @@ public class MissingPersonFragment extends Fragment {
     FloatingActionButton bAddMissingPerson;
     RecyclerView.Adapter adaptor;
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -41,7 +46,7 @@ public class MissingPersonFragment extends Fragment {
         bAddMissingPerson = (FloatingActionButton) view.findViewById(R.id.bAddMissingPerson);
         search = (EditText) view.findViewById( R.id.search);
 
-
+        ((AppCompatActivity) getActivity()).getSupportActionBar();
 
 
         bAddMissingPerson.setOnClickListener(new View.OnClickListener() {
@@ -73,11 +78,82 @@ public class MissingPersonFragment extends Fragment {
         adaptor = new MyRecycleAdapterMissingPerson(view.getContext(),myDataSet, Designations);
         rv.setAdapter(adaptor);
 
+        addTextListener(myDataSet, Designations);
+
         return view;
+        
+
 
 
     }
 
+    private void addTextListener(final String[] myDataSet, final String[] designations) {
+
+        final String[] filteredDataSet = null;
+        final String[] filteredDesignations = null;
 
 
+
+        if (myDataSet != null || designations != null) {
+
+
+            search.addTextChangedListener(new TextWatcher() {
+
+
+                public void afterTextChanged(Editable s) {
+                }
+
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                public void onTextChanged(CharSequence query, int start, int before, int count) {
+
+                    query = query.toString().toLowerCase();
+
+                    final List<String> filteredList = new ArrayList<>();
+
+
+                    for (int i = 0; i <myDataSet.length; i++) {
+
+                        final String text1 = myDataSet[i].toLowerCase();
+                        if (text1.contains(query)) {
+
+                            filteredList.add(myDataSet[i]);
+
+                        }
+                    }
+                    try{
+                        filteredDataSet[filteredList.size() - 1] =  filteredList.toString();
+                    }catch (Exception e){
+                        Toast.makeText(view.getContext(), "Error Error", Toast.LENGTH_LONG);
+                    }
+
+
+                    for (int i = 0; i < designations.length; i++) {
+
+                        final String text1 = designations[i].toLowerCase();
+                        if (text1.contains(query)) {
+
+                            filteredList.add(designations[i]);
+                        }
+                    }
+
+                    filteredDesignations[filteredList.size()] = filteredList.toString();
+
+                    rv.setLayoutManager(new LinearLayoutManager(view.getContext()));
+                    adaptor = new MyRecycleAdapterMissingPerson(view.getContext(),filteredDataSet, filteredDesignations);
+                    //adaptor = new MyRecycleAdapterMissingPerson(filteredList, view.getContext());
+                    rv.setAdapter(adaptor);
+                    adaptor.notifyDataSetChanged();  // data set changed
+                }
+            });
+        }
+
+
+    }
+
+    @Override
+    public void setHasOptionsMenu(boolean hasMenu) {
+        super.setHasOptionsMenu(hasMenu);
+    }
 }
