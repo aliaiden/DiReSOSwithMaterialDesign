c39c349804b08a7115d26d2ed13a2f4216dc27c2
aliaiden <aliaiden@yahoo.com>
2017-01-02 14:48:28 +0500

    Added Home fragment, login and token button are not visible now.

--- app/src/main/java/com/diresos/alihaider/logreg2/HomeActivity.java
+++ app/src/main/java/com/diresos/alihaider/logreg2/HomeActivity.java
@@ -1,40 +1,39 @@
 package com.diresos.alihaider.logreg2;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
+    private static final boolean TODO = true;
     NavigationView navigationView;
     Toolbar toolbar;
     Button dashboard, sendNot;
@@ -43,7 +42,12 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final android.app.FragmentManager myFrag = getFragmentManager();
+
         setContentView(R.layout.activity_home);
+        myFrag.beginTransaction().replace(R.id.fLayout, new HomeFragment()).commit();
+
 
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -63,14 +67,18 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
 
-                android.app.FragmentManager myFrag = getFragmentManager();
 
 
                 switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        myFrag.beginTransaction().replace(R.id.fLayout, new HomeFragment()).commit();
+                        // Intent i = new Intent(HomeActivity.this, PostActivity.class);
+                        //startActivity(i);
 
+                        break;
                     case R.id.nav_quick_post:
                         myFrag.beginTransaction().replace(R.id.fLayout, new QuickPostFragment()).commit();
-                       // Intent i = new Intent(HomeActivity.this, PostActivity.class);
+                        // Intent i = new Intent(HomeActivity.this, PostActivity.class);
                         //startActivity(i);
 
                         break;
@@ -93,6 +101,35 @@ public class HomeActivity extends AppCompatActivity {
 
                     case R.id.nav_events:
                         myFrag.beginTransaction().replace(R.id.fLayout, new EventsFragment()).commit();
+                        break;
+                    case R.id.nav_website:
+                        myFrag.beginTransaction().replace(R.id.fLayout, new EventsFragment()).commit();
+                        break;
+                    case R.id.nav_email:
+                        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                                "mailto", "info@dirsos.com", null));
+                        startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+                        break;
+                    case R.id.nav_sms:
+
+                        showSMSDialog();
+
+                        break;
+                    case R.id.nav_call:
+                        Intent callIntent = new Intent(Intent.ACTION_CALL);
+                        callIntent.setData(Uri.parse("tel:03009203159"));
+                        if (ActivityCompat.checkSelfPermission(getApplication(), android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            return TODO;
+                        }
+                        startActivity(callIntent);
+
                         break;
                 }
                 return true;
@@ -157,15 +194,38 @@ public class HomeActivity extends AppCompatActivity {
                     }
                 };
                 MySingleton.getInstance(HomeActivity.this).addToRequestQueue(stringRequest);
-
-
             }
         });
-
-
     }
 
+    private void showSMSDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.sms_dialog, null);
+        dialogBuilder.setView(dialogView);
 
+        final EditText smsMessage = (EditText) dialogView.findViewById(R.id.etSMSmessage);
+        dialogBuilder.setTitle("Send us an SMS");
+        dialogBuilder.setPositiveButton("SEND", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                sendSMS(smsMessage.getText().toString());
+            }
+        });
+        AlertDialog b = dialogBuilder.create();
+        b.show();
+    }
+
+    private void sendSMS(String s) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage("+923009203159", null, s, null, null);
+            Toast.makeText(getApplicationContext(), "Message sent", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Failed, please try again", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
 
 
 }
---
+++ app/src/main/java/com/diresos/alihaider/logreg2/HomeFragment.java
@@ -0,0 +1,74 @@
+package com.diresos.alihaider.logreg2;
+
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by ali on 1/1/17.
+ */
+public class HomeFragment extends Fragment {
+
+    int delay = 0; // delay for 0 sec.
+    int period = 2000; // repeat every 25 seconds.
+    Timer timer = new Timer();
+
+
+
+    int gallery_grid_Images[]={R.drawable.home_flipper_image1,R.drawable.home_flipper_image2,R.drawable.home_flipper_image3,
+            R.drawable.home_flipper_image4,R.drawable.home_flipper_image5};
+
+    View view;
+    ViewFlipper viewFlipper;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        viewFlipper = (ViewFlipper) view.findViewById(R.id.flipper);
+        for(int i=0;i<gallery_grid_Images.length;i++)
+        {
+            //  This will create dynamic image view and add them to ViewFlipper
+            setFlipperImage(gallery_grid_Images[i]);
+        }
+        viewFlipper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewFlipper.showNext();
+
+            }
+        });
+
+
+
+
+        return view;
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void setFlipperImage(int res) {
+        Log.i("Set Filpper Called", res+"");
+        ImageView image = new ImageView(getActivity());
+        image.setBackgroundResource(res);
+        viewFlipper.addView(image);
+    }
+
+    public class SampleTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            //MAKE YOUR LOGIC TO SET IMAGE TO IMAGEVIEW
+        }
+    }
+}
